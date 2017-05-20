24eb0c0f16809811c121382aa7e84d9e92f2caba
abe1ge <greenfire@live.co.uk>
2017-05-20 13:57:33 +0100

    4.8 practival jquery, side menu bar toggle working,

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -1,23 +1,31 @@
-﻿(function () {
+﻿(function () { 
+    //var ele = $("#username");
+    //ele.text("Brook");
 
+    //var main = $("#main");
 
+    //main.on("mouseenter",  function () {
+    //    main.style = "background-color: #888;";
+    //});
 
-    var ele = $("#username");
-    ele.text("Brook");
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
 
-    var main = $("#main");
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
 
-    main.on("mouseenter",  function () {
-        main.style = "background-color: #888;";
-    });
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
 
-    main.on("mouseleave", function () {
-        main.style = "";
-    });
-
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function () {
-        var me = $(this);
-        alert(me.text());
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     });
 })();
