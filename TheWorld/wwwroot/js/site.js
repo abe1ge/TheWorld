e1bb0868bf95fa67464ac1b11f2e81096244b598
abe1ge <greenfire@live.co.uk>
2017-05-20 12:07:22 +0100

    4.5 using global scope for js function

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -1,12 +1,14 @@
-﻿var ele = document.getElementById("username");
-ele.innerHTML = "Brook";
+﻿(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Brook";
 
-var main = document.getElementById("main");
+    var main = document.getElementById("main");
 
-main.onmouseenter = function () {
-    main.style.backgroundColor = "#888";
-};
+    main.onmouseenter = function () {
+        main.style.backgroundColor = "#888";
+    };
 
-main.onmouseleave = function () {
-    main.style.backgroundColor = "";
-};
+    main.onmouseleave = function () {
+        main.style.backgroundColor = "";
+    };
+})();
