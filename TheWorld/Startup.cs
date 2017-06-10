a76b7677908c1a0c10c35a74e2fb71f3e8f3e261
abe1ge <greenfire@live.co.uk>
2017-06-10 17:07:48 +0100

    5.6 Using Tag Helpers, changing page links so they can be found no matter what

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -16,19 +16,35 @@ namespace TheWorld
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            //loggerFactory.AddConsole();
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseDefaultFiles();
+            
+
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", Action = "Index" }
+                    );
+            });
         }
     }
 }
