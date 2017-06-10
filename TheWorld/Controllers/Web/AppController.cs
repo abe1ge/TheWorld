72e126bf4e36c4289eabe3e29fd80f6a1d260598
abe1ge <greenfire@live.co.uk>
2017-06-10 18:50:18 +0100

    5.10 Adding a Service - Adding the mail Services, the mail service is just a test so changed the set-up file so it is called only on dev or testing machines but if not and the real mail service is implemented that should be used

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -4,12 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheWorld.Services;
 using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -24,6 +32,7 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
+            _mailService.SendMail("abel@aol.com", model.Email, "From The World", model.Message);
             return View();
         }
 
