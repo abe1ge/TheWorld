66bfb1b9cd019f7a607ff4df924e11d4f6743efc
DESKTOP-V43RM04\Abelether Germa <abelge@github.com>
2017-06-20 07:27:02 +0100

    7.4 Using the DbContext - it doesn't seem to work, getting a differnt exception then expected. will move on to see what happens

--- TheWorld/Models/Trip.cs
+++ TheWorld/Models/Trip.cs
@@ -8,6 +8,11 @@ namespace TheWorld.Models
 {
     public class Trip
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string UserName { get; set; }
+
+        public ICollection<Stop> Strops { get; set; }
     }
 }
